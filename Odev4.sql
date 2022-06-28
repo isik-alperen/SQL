618ce7c016145b593bc08d4f71d7eeb579ced060
Alperen <104639734+isik-alperen@users.noreply.github.com>
2022-06-28 15:20:52 +0300

    Create Odev4.sql

---
+++ Odev4.sql
@@ -0,0 +1,13 @@
+-- I write all code requested from me in the form of comments. 
+--You can delete the comment operator of the code you want to run and run it.
+
+
+--SELECT DISTINCT replacement_cost FROM film; 
+
+--SELECT COUNT (DISTINCT replacement_cost) FROM film ;
+
+--SELECT COUNT (*) FROM film WHERE title ILIKE 'T%' AND rating ='G';
+
+--SELECT COUNT (*) FROM country WHERE country LIKE '_____';
+
+--SELECT COUNT (*) FROM city WHERE city ILIKE '%r';
