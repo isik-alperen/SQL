402b5d29a82316c35dbd2d87d46a01a48c286228
Alperen <104639734+isik-alperen@users.noreply.github.com>
2022-06-28 14:43:53 +0300

    Create ODEV3.sql

---
+++ ODEV3.sql
@@ -0,0 +1,10 @@
+-- I write all code requested from me in the form of comments. 
+--You can delete the comment operator of the code you want to run and run it.
+
+--SELECT * FROM country WHERE country ~~ 'A%a';
+
+--SELECT * FROM country WHERE country LIKE '_____%n';
+
+--SELECT * FROM film WHERE title ILIKE '%T%%T%%T%%T%';
+
+--SELECT * FROM film WHERE title LIKE 'C%' AND length >90 AND rental_rate=2.99;
