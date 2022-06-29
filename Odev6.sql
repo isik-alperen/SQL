868c92429956191e1fb45e69f516bb1537b8ff68
Alperen <104639734+isik-alperen@users.noreply.github.com>
2022-06-29 09:58:03 +0300

    Create Odev6.sql

---
+++ Odev6.sql
@@ -0,0 +1,10 @@
+-- I write all code requested from me in the form of comments. 
+--You can delete the comment operator of the code you want to run and run it.
+
+--SELECT ROUND(AVG(rental_rate),3) FROM film;
+
+--SELECT COUNT (*) FROM film WHERE title LIKE 'C%';
+
+--SELECT MAX(length) FROM film WHERE rental_rate =0.99;
+
+--SELECT COUNT(DISTINCT replacement_cost) FROM film WHERE length>150;
