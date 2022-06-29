b20c7cc2e3eb545004fe1a9dc63201f11acb70b6
Alperen <104639734+isik-alperen@users.noreply.github.com>
2022-06-29 10:52:42 +0300

    Create Odev7.sql

---
+++ Odev7.sql
@@ -0,0 +1,10 @@
+-- I write all code requested from me in the form of comments. 
+--You can delete the comment operator of the code you want to run and run it.
+
+--SELECT rating, COUNT(*) FROM film GROUP BY  rating;
+
+--SELECT COUNT(*) , replacement_cost FROM film GROUP BY replacement_cost HAVING (COUNT(*)>50
+
+--SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+
+--SELECT country_id, COUNT(*)  from city GROUP BY country_id ORDER BY count DESC LIMIT 1;
